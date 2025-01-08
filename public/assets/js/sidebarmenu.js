901bee51f8bdf56bb2f00b17f9220cb6201189da
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-01-08 14:32:10 +0700

    update sidebar menu already active when subdomain or filter active

--- public/assets/js/sidebarmenu.js
+++ public/assets/js/sidebarmenu.js
@@ -1,52 +1,51 @@
-/*
-Template Name: Admin Template
-Author: Wrappixel
-
-File: js
-*/
-// ==============================================================
-// Auto select left navbar
-// ==============================================================
 $(function () {
     "use strict";
-    var url = window.location + "";
-    var path = url.replace(
-      window.location.protocol + "//" + window.location.host + "/",
-      ""
-    );
+
+    // Get the current URL and extract the base path
+    var currentUrl = window.location.href.split("?")[0]; // URL without query string
+    var baseUrl = window.location.origin;
+
+    // Match sidebar links to the current URL (including query strings)
     var element = $("ul#sidebarnav a").filter(function () {
-      return this.href === url || this.href === path; // || url.href.indexOf(this.href) === 0;
+        var linkUrl = this.href.split("?")[0]; // Compare without query strings
+        return (
+            linkUrl.startsWith(baseUrl) && currentUrl === linkUrl // Ensure base path matches
+        );
     });
-    element.parentsUntil(".sidebar-nav").each(function (index) {
-      if ($(this).is("li") && $(this).children("a").length !== 0) {
-        $(this).children("a").addClass("active");
-        $(this).parent("ul#sidebarnav").length === 0
-          ? $(this).addClass("active")
-          : $(this).addClass("selected");
-      } else if (!$(this).is("ul") && $(this).children("a").length === 0) {
-        $(this).addClass("selected");
-      } else if ($(this).is("ul")) {
-        $(this).addClass("in");
-      }
+
+    // Activate the matched elements and their parent items
+    element.parentsUntil(".sidebar-nav").each(function () {
+        if ($(this).is("li") && $(this).children("a").length !== 0) {
+            $(this).children("a").addClass("active");
+            $(this).parent("ul#sidebarnav").length === 0
+                ? $(this).addClass("active")
+                : $(this).addClass("selected");
+        } else if (!$(this).is("ul") && $(this).children("a").length === 0) {
+            $(this).addClass("selected");
+        } else if ($(this).is("ul")) {
+            $(this).addClass("in");
+        }
     });
-  
+
     element.addClass("active");
+
+    // Handle click events for toggling
     $("#sidebarnav a").on("click", function (e) {
-      if (!$(this).hasClass("active")) {
-        // hide any open menus and remove all other classes
-        $("ul", $(this).parents("ul:first")).removeClass("in");
-        $("a", $(this).parents("ul:first")).removeClass("active");
-  
-        // open our new menu and add the open class
-        $(this).next("ul").addClass("in");
-        $(this).addClass("active");
-      } else if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        $(this).parents("ul:first").removeClass("active");
-        $(this).next("ul").removeClass("in");
-      }
+        if (!$(this).hasClass("active")) {
+            $("ul", $(this).parents("ul:first")).removeClass("in");
+            $("a", $(this).parents("ul:first")).removeClass("active");
+
+            $(this).next("ul").addClass("in");
+            $(this).addClass("active");
+        } else if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).parents("ul:first").removeClass("active");
+            $(this).next("ul").removeClass("in");
+        }
     });
-    $("#sidebarnav >li >a.has-arrow").on("click", function (e) {
-      e.preventDefault();
+
+    // Prevent default behavior for items with sub-menus
+    $("#sidebarnav > li > a.has-arrow").on("click", function (e) {
+        e.preventDefault();
     });
-  });
+});
