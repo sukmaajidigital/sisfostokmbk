286914b9b2fb9631b5beee4393e7f2854573e8a7
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-01-08 15:17:16 +0700

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,17 +6,17 @@
                     <i class="ti ti-menu-2"></i>
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link nav-icon-hover" href="javascript:void(0)">
-                    <i class="ti ti-bell-ringing"></i>
-                    <div class="notification bg-warning rounded-circle"></div>
-                </a>
-            </li>
         </ul>
+
         <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+                <div>
+                    <h6 class="mb-0 text-dark">Hallo, {{ Auth::user()->name }}</h6>
+                </div>
                 <li class="nav-item dropdown">
                     <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown" aria-expanded="false">
+
                         <img src="{{ asset('assets/images/profile/user-1.jpg') }}" alt="" width="35" height="35" class="rounded-circle">
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
